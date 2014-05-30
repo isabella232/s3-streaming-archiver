cefa34f10f96d218d044b1c5b927c59388b252f4
Jordan Sitkin <jordan@nuts.com>
2014-05-30 16:44:47 -0700

    does timestamp checking to avoid rebuilding archinve when not needed

--- archive.js
+++ archive.js
@@ -3,6 +3,7 @@ var mpu = require("knox-mpu");
 var s3lister = require("s3-lister");
 var async = require("async");
 var archiver = require("archiver");
+var moment = require("moment");
 
 var sourceBucketList = process.env.S3_SOURCE_BUCKET.split(",");
 var targetClient = knox.createClient({
@@ -13,9 +14,19 @@ var targetClient = knox.createClient({
 
 async.eachSeries(sourceBucketList, function(bucket, bucketListCallback) {
 
-  // TODO check updated_at timestamp for most recent item in bucket.
-  // compare against timestamp on archive in targetBucket. if existing
-  // archive is newer than source bucket files, dont do anything!
+  var archiveFileName = bucket + "-archive.tar",
+      archiveNeedsUpdate = false,
+      archiveFileUpdatedAt;
+
+  targetClient.headFile(archiveFileName, function(err, res) {
+    if (err) {
+      console.log(err);
+      archiveFileUpdatedAt = moment().subtract('days', 365);
+    } else {
+      console.log(archiveFileName, "was last created on", res.headers['last-modified']);
+      archiveFileUpdatedAt = Date.parse(res.headers['last-modified']);
+    }
+  });
 
   var sourceClient = knox.createClient({
     key: process.env.S3_ACCESS_KEY_ID,
@@ -27,66 +38,78 @@ async.eachSeries(sourceBucketList, function(bucket, bucketListCallback) {
   var keys = [];
 
   lister.on('data', function(data) {
-    if (data.Key.indexOf(".tar") === -1) {
-      keys.push(data.Key);
+    if (!archiveNeedsUpdate) {
+      if (moment(Date.parse(data['last-modified'])).isAfter(archiveFileUpdatedAt)) {
+        archiveNeedsUpdate = true;
+      }
     }
+    keys.push(data.Key);
   });
 
+
   lister.on('end', function() {
-    console.log('==> Done!, looping over '+keys.length+' keys...');
 
-    var tarStream = archiver('tar');
-    var upload = new mpu({
-      client: targetClient,
-      objectName: bucket + '-archive.tar',
-      stream: tarStream,
-      maxRetries: 4
-    }, function(err, body) {
-      console.log("Upload callback", err, body);
-      bucketListCallback();
-    });
-    upload.on('uploading', console.log);
-    upload.on('uploaded', console.log);
-    upload.on('error', console.log);
-    upload.on('initiated', console.log);
-    upload.on('completed', function(a) {
-      console.log("Completed Upload ", a);
-    });
+    if (archiveNeedsUpdate) {
 
-    // for testing:
-    // keys = keys.slice(1, 1000);
+      console.log('==> Found newer file(s)! looping over '+keys.length+' keys...');
 
-    var tarStreamQueue = async.queue(function(fileObject, callback) {
-      tarStream.append(fileObject.stream, { name: fileObject.name });
-      callback();
-    }, 1);
-
-    var downloadQueue = async.queue(function(key, callback) {
-      // process.stdout.write(key + " .:. ");
-      process.stdout.write(".");
-      sourceClient.getFile(key, function(err, res) {
-        if (err) {
-          console.log("client.getFile Error on ", key, err);
-          downloadQueue.push(key); // try again
-          callback();
-        } else {
-          tarStreamQueue.push({ stream: res, name: key } );
-          res.on("end", callback);
-        }
+      var tarStream = archiver('tar');
+      var upload = new mpu({
+        client: targetClient,
+        objectName: archiveFileName,
+        stream: tarStream,
+        maxRetries: 4
+      }, function(err, body) {
+        console.log("Upload callback", err, body);
+        bucketListCallback();
       });
-    }, 20);
+      upload.on('uploading', console.log);
+      upload.on('uploaded', console.log);
+      upload.on('error', console.log);
+      upload.on('initiated', console.log);
+      upload.on('completed', function(a) {
+        console.log("Completed Upload ", a);
+      });
 
-    downloadQueue.push(keys);
+      // for testing:
+      // keys = keys.slice(1, 1000);
 
-    var finalizeTarStream = function() {
-      if (tarStreamQueue.idle() && downloadQueue.idle()) {
-        console.log("Finalizing tarStream");
-        tarStream.finalize();
-      }
-    };
+      var tarStreamQueue = async.queue(function(fileObject, callback) {
+        tarStream.append(fileObject.stream, { name: fileObject.name });
+        callback();
+      }, 1);
 
-    tarStreamQueue.drain = finalizeTarStream;
-    downloadQueue.drain = finalizeTarStream;
+      var downloadQueue = async.queue(function(key, callback) {
+        // process.stdout.write(key + " .:. ");
+        process.stdout.write(".");
+        sourceClient.getFile(key, function(err, res) {
+          if (err) {
+            console.log("client.getFile Error on ", key, err);
+            downloadQueue.push(key); // try again
+            callback();
+          } else {
+            tarStreamQueue.push({ stream: res, name: key } );
+            res.on("end", callback);
+          }
+        });
+      }, 20);
+
+      downloadQueue.push(keys);
+
+      var finalizeTarStream = function() {
+        if (tarStreamQueue.idle() && downloadQueue.idle()) {
+          console.log("Finalizing tarStream");
+          tarStream.finalize();
+        }
+      };
+
+      tarStreamQueue.drain = finalizeTarStream;
+      downloadQueue.drain = finalizeTarStream;
+
+    } else {
+      console.log("==> Looks like all files are older than the archive. Skipping.");
+      bucketListCallback();
+    }
   });
 
   lister.on('error', function(err) {
